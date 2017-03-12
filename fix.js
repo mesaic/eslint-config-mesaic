b969d766bb32289b95aa9ea8673df70bde66b63f
Jonathan Werner <jwerner@posteo.de>
2017-03-12 14:44:17 +0100

    add more fix rules

--- fix.js
+++ fix.js
@@ -7,5 +7,7 @@ module.exports = {
   rules: {
     'arrow-parens': [2, 'always'],
     'jsx-quotes': [2, 'prefer-single'],
+    'react/jsx-closing-bracket-location': [2, {selfClosing: 'after-props', nonEmpty: 'after-props'}],
+    'react/jsx-space-before-closing': [2, 'always'],
   },
 }
