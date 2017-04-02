7c5c4c35f1c0745bd9398e449297bf428b861dc3
Jonathan Werner <jwerner@posteo.de>
2017-04-02 11:37:50 +0200

    add prototyping.js
    For quick prototypes, with minimal set of rules.

---
+++ prototyping.js
@@ -0,0 +1,22 @@
+{
+  "extends": [
+    "./slim.js"
+  ],
+  "rules": {
+    "flowtype/require-valid-file-annotation": 0,
+    "flowtype/require-return-type": 0,
+    "flowtype/require-parameter-type": 0,
+    "no-console": 0,
+    "import/order": 0,
+    "import/first": 0,
+    "import/newline-after-import": 0,
+    "import/imports-first": 0,
+    "no-restricted-syntax": 0,
+    "no-await-in-loop": 0,
+    "no-unused-vars": 0,
+    "camelcase": 0
+  },
+  "globals": {
+    "test": false
+  }
+}
